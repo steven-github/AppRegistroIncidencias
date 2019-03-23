18515a9200dd873268cf494ed8f223a03a5342f9
Steven S <freelancers2010@gmail.com>
2019-03-22 19:25:51 -0600

    - Reset register form functionalities

--- app/src/main/java/ulacit/ed/appregistroincidencias/RegistroUsuarioActivity.java
+++ app/src/main/java/ulacit/ed/appregistroincidencias/RegistroUsuarioActivity.java
@@ -4,14 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
+
+    // Get the widgets reference from XML layout
+    private EditText txtCedula, txtNombre, txtProvincia, txtEmail;
+    Button btn;
+    RadioGroup rdSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtCedula = (EditText) findViewById(R.id.txtCedula);
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtProvincia = (EditText) findViewById(R.id.txtProvincia);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        rdSex = (RadioGroup) findViewById(R.id.radioSex);
+        btn = (Button) findViewById(R.id.reset);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Clear the second EditText
+                txtCedula.getText().clear();
+                txtNombre.getText().clear();
+                txtProvincia.getText().clear();
+                txtEmail.getText().clear();
+                rdSex.clearCheck();
+            }
+        });
+
     }
 
     @Override
